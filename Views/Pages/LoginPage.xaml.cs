bb4053c39794f9074e322c7880ccde41db8c43a0
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-03 11:44:05 +0000

    feat(loginpage): markup the page

---
+++ Views/Pages/LoginPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace UserPaymentsDesktopApp.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/Windows/NavigationWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using UserPaymentsDesktopApp.Views.Pages;
+
+namespace UserPaymentsDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class NavigationWindow : Window
+    {
+        public NavigationWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new LoginPage());
+        }
+    }
+}
