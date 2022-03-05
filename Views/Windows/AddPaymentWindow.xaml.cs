6909bae7a58171a6c97adf7fe07cfee62ce6b7fc
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-05 15:27:17 +0000

    feat(paymentspage): add saving a new payment

--- Views/Windows/AddPaymentWindow.xaml.cs
+++ Views/Windows/AddPaymentWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using UserPaymentsDesktopApp.Models.Entities;
+using UserPaymentsDesktopApp.Services;
 
 namespace UserPaymentsDesktopApp.Views.Windows
 {
@@ -10,9 +15,13 @@ namespace UserPaymentsDesktopApp.Views.Windows
     /// </summary>
     public partial class AddPaymentWindow : Window
     {
+        public PaymentOfUser CurrentPayment { get; set; }
+            = new PaymentOfUser();
+        public PaymentType CurrentType { get; set; }
         public AddPaymentWindow()
         {
             InitializeComponent();
+            DataContext = this;
             _ = LoadCategoriesAsync();
         }
 
@@ -21,7 +30,7 @@ namespace UserPaymentsDesktopApp.Views.Windows
         /// </summary>
         private async Task LoadCategoriesAsync()
         {
-            ComboCategories.ItemsSource = await Task.Run(() =>
+            List<PaymentType> categories = await Task.Run(() =>
             {
                 using (UserPaymentsBaseEntities context =
                     new UserPaymentsBaseEntities())
@@ -29,6 +38,80 @@ namespace UserPaymentsDesktopApp.Views.Windows
                     return context.PaymentType.ToList();
                 }
             });
+            ComboCategories.ItemsSource = categories;
+            ComboCategories.SelectedItem = categories.First();
+        }
+
+        /// <summary>
+        /// Сохраняет платёж в базе данных.
+        /// </summary>
+        private async void SavePaymentAsync(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (CurrentPayment == null)
+            {
+                _ = errors.AppendLine("Укажите тип платежа");
+            }
+            if (string.IsNullOrWhiteSpace(PaymentName.Text))
+            {
+                _ = errors.AppendLine("Укажите наименование платежа");
+            }
+            if (string.IsNullOrWhiteSpace(PaymentCount.Text)
+                || !int.TryParse(PaymentCount.Text, out _)
+                || int.Parse(PaymentCount.Text) <= 0)
+            {
+                _ = errors.AppendLine("Количество - это положительное целое число");
+            }
+            if (string.IsNullOrWhiteSpace(Price.Text)
+              || !decimal.TryParse(Price.Text, out _)
+              || decimal.Parse(Price.Text) <= 0)
+            {
+                _ = errors.AppendLine("Цена - это десятичное положительное число");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBoxService.Warn(errors.ToString());
+                return;
+            }
+            CurrentPayment.UserId = (App.Current as App)
+                .CurrentUser
+                .Id;
+            CurrentPayment.PaymentTypeId = CurrentType.Id;
+            CurrentPayment.PaymentDate = DateTime.Now;
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    using (UserPaymentsBaseEntities context =
+                    new UserPaymentsBaseEntities())
+                    {
+                        _ = context.PaymentOfUser.Add(CurrentPayment);
+                        _ = context.SaveChanges();
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+                MessageBoxService.Inform("Не удалось сохранить платёж. " +
+                    "Проверьте подключение к базе данных");
+                return;
+            }
+            MessageBoxService.Inform("Платёж успешно сохранён!");
+            Close();
+        }
+
+        /// <summary>
+        /// Отменяет создание платежа.
+        /// </summary>
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            if (MessageBoxService.Ask("Отменить создание " +
+                "платежа?"))
+            {
+                Close();
+            }
         }
     }
 }
